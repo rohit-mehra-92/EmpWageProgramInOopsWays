3479b4fdcd7d4945662a334cb9be5c38d5c0f2e7
Rohit Singh Mehra <rohit264cool@gmail.com>
2022-03-29 17:06:11 +0530

    Added:Calculated employee wage on halfday

--- src/Employee.java
+++ src/Employee.java
@@ -1,14 +1,24 @@
-public class Employee {
-    public static final int FULL_DAY_HOUR=8;
-    public static final int WAGE_PER_HOUR=20;
+public class Employee<workHour> {
+    public static final int WAGE_PER_HOUR = 20;
 
     public int isPresent() {
-        int empCheck = (int) (Math.random() * 2);
+        int empCheck = (int) (Math.random() * 3);
         return empCheck;
     }
 
-    public int empDailyWage(){
-        int dailyWage = FULL_DAY_HOUR * WAGE_PER_HOUR;
-        return dailyWage;
+    public int empDailyWage() {
+        int workHour = 0;
+        if (isPresent() == 2) {
+            System.out.println("Employee worked full day");
+            workHour = 8;
+        } else if (isPresent() == 1) {
+            System.out.println("Employee worked half day");
+            workHour = 4;
+        } else {
+            System.out.println("Employee is absent");
+            workHour = 0;
+        }
+        int empDailyWage = workHour * WAGE_PER_HOUR;
+        return empDailyWage;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,8 @@ public class Main {
         System.out.println();
         Employee emp = new Employee();
 
-        int value = emp.isPresent();
-        if (value == 1) {
-            System.out.println("Employee is Present");
-            System.out.println("Employee daily wage : $" + emp.empDailyWage());
-        } else {
-            System.out.println("Employee is Absent");
-            System.out.println("Employee gets no wage");
-        }
+        int wage = emp.empDailyWage();
+        System.out.println("Employee daily Wage : $" + wage);
+
     }
 }
