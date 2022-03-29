0aa53e3a79ccc59207e634924e223885adbd0b73
Rohit Singh Mehra <rohit264cool@gmail.com>
2022-03-29 16:05:58 +0530

    Added:Calculated employee daily wage

--- src/Employee.java
+++ src/Employee.java
@@ -1,8 +1,14 @@
 public class Employee {
+    public static final int FULL_DAY_HOUR=8;
+    public static final int WAGE_PER_HOUR=20;
 
     public int isPresent() {
         int empCheck = (int) (Math.random() * 2);
         return empCheck;
     }
 
+    public int empDailyWage(){
+        int dailyWage = FULL_DAY_HOUR * WAGE_PER_HOUR;
+        return dailyWage;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage computation program");
         System.out.println();
@@ -7,7 +8,10 @@ public class Main {
         int value = emp.isPresent();
         if (value == 1) {
             System.out.println("Employee is Present");
-        } else
+            System.out.println("Employee daily wage : $" + emp.empDailyWage());
+        } else {
             System.out.println("Employee is Absent");
+            System.out.println("Employee gets no wage");
+        }
     }
 }
